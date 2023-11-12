a4d1b91d69b0cb3905fd1e70efb8fb09bf7813cf
Kimmo Saari <kimmo.saari@fennicagaming.com>
2023-11-12 21:18:04 +0200

    Fix

--- uninstall.sh
+++ uninstall.sh
@@ -19,23 +19,24 @@ minor_step "Disabling mjpg_streamer_${SERVICE} startup daemon"
 sudo systemctl disable "mjpg_streamer_${SERVICE}.service"
 continue_if_succeeded
 
-minor_step "Killing mjpg_streamer process"
-kill -9 "$(ps aSux | pgrep mjpg_streamer)"
-
 minor_step "Deleting repository"
 rm -rf mjpg-streamer
 
 minor_step "Deleting mjpg_streamer_${SERVICE}.service"
-rm "/etc/systemd/system/mjpg_streamer_${SERVICE}.service"
+sudo rm "/etc/systemd/system/mjpg_streamer_${SERVICE}.service"
 
 minor_step "Deleting startup_${SERVICE}.sh"
 rm "startup_${SERVICE}.sh"
 
-step "Removing dependencies"
 minor_step "Do you want to remove the 'cmake libjpeg8-dev' dependencies as well? [y/N]: "
 read -n1 remove_dependencies
+
+step "Removing dependencies"
 if [[ "${remove_dependencies}" == "y" ]]; then
+  minor_step "Deleting dependencies..."
   sudo apt remove cmake libjpeg8-dev -y
+else
+  minor_step "Skipped!"
 fi
 
 all_done
