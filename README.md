df42b64097875f810dba0eadc589c6005447a200
Kimmo Saari <kimmo.saari@fennicagaming.com>
2023-11-12 21:16:23 +0200

    Updates

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ v4l2-ctl --list-devices
 ./install.sh   "/dev/video2"     "1920x1080"  "30"  "100"     "8080" "0"          "20" # Creates service for video0 with resolution 1920x1080, 30fps, quality=100, auto focus disabled, focus set to 20 and in port 8080
 ./install.sh   "/dev/video0"     "1280x960"   "30"  "100"     "8081" "0"          "20" # Creates service for video2 with resolution 1280x960,  30fps, quality=100, auto focus enabled,  focus set to 20 and in port 8081
 ```
+
+## Uninstall
+
+```bash
+./uninstall.sh /dev/video0
+```
--- uninstall.sh
+++ uninstall.sh
@@ -3,20 +3,20 @@
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 BASH_DIR="${ROOT_DIR}/bash"
 
+CAMERA_DEVICE=${1:-"/dev/video0"}
+SERVICE=${CAMERA_DEVICE##*/}
+
 # shellcheck source=/dev/null
 source "${BASH_DIR}/_main.sh"
 
-step "Removing dependencies"
-sudo apt remove cmake libjpeg8-dev -y
-
 step "mjpg-streamer"
 
-minor_step "Stopping daemon"
-sudo systemctl stop mjpg_streamer
+minor_step "Stopping mjpg_streamer_${SERVICE} daemon"
+sudo systemctl stop "mjpg_streamer_${SERVICE}.service"
 continue_if_succeeded
 
-minor_step "Disabling startup daemon"
-sudo systemctl disable mjpg_streamer
+minor_step "Disabling mjpg_streamer_${SERVICE} startup daemon"
+sudo systemctl disable "mjpg_streamer_${SERVICE}.service"
 continue_if_succeeded
 
 minor_step "Killing mjpg_streamer process"
@@ -24,5 +24,18 @@ kill -9 "$(ps aSux | pgrep mjpg_streamer)"
 
 minor_step "Deleting repository"
 rm -rf mjpg-streamer
+
+minor_step "Deleting mjpg_streamer_${SERVICE}.service"
+rm "/etc/systemd/system/mjpg_streamer_${SERVICE}.service"
+
+minor_step "Deleting startup_${SERVICE}.sh"
+rm "startup_${SERVICE}.sh"
+
+step "Removing dependencies"
+minor_step "Do you want to remove the 'cmake libjpeg8-dev' dependencies as well? [y/N]: "
+read -n1 remove_dependencies
+if [[ "${remove_dependencies}" == "y" ]]; then
+  sudo apt remove cmake libjpeg8-dev -y
+fi
 
 all_done
